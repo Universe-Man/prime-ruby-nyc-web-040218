446bfb779d05367220b74162613e5932120f5c6e
Ian Pollack <ianjpollack@gmail.com>
2018-03-20 22:25:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  n = number.to_i
+  if n == 2
+    return true
+  elsif n <= 1
+    return false
+  end
+  i = 2
+  until i == n
+    equation = (n % i)
+    if equation == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
