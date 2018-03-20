6387dc2ca10cc62623e524ef7851bb984284f15a
Ian Pollack <ianjpollack@gmail.com>
2018-03-20 22:17:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+require "pry"
+
+def prime?(number)
+  m = number.to_i
+  n = m.to_f
+  if n == 2
+    return true
+  elsif n <= 1
+    return false
+  end
+  i = 2
+  until i == n
+    equation = (n % i)
+binding.pry
+    if equation == 0
+      break
+      return false
+    end
+    i += 1
+  end
+  return true
+end
